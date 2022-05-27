9811b93cae955a09f31e797c8deca3c3b4ac0cc1
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-27 14:43:41 -0400

    eventListener to submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-var stockDataApiToken = "VQxLXn7DNMOmzB4lAV4OxmImM24GRRLXLHOfAZtv";
 var stockDataSymbolPlaceholder = "TSLA,APPL"
+var userStockFormEl = document.querySelector("#stockForm1");
+var stockNameInputEl = document.querySelector("#inline-stock-name");
+var holdingsInputEl = document.querySelector("#inline-shares");
 
 var getUserStock = function(userStock) {
     // format the api url
-    var stockDataApiUrl = "https://api.stockdata.org/v1/data/quote?symbols=" + stockDataSymbolPlaceholder + "&api_token=" + stockDataApiToken ;
+    var stockDataApiUrl = "https://api.stockdata.org/v1/data/quote?symbols=" + stockDataSymbolPlaceholder + "&api_token=VQxLXn7DNMOmzB4lAV4OxmImM24GRRLXLHOfAZtv" ;
 
     // make a request to the url
     fetch(stockDataApiUrl)
@@ -14,4 +16,9 @@ var getUserStock = function(userStock) {
         });
 };
 
-getUserStock();
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(">>>" , event);
+};
+
+userStockFormEl.addEventListener("submit" , formSubmitHandler);
