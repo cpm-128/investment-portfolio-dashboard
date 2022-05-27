ab72f3d3facc29d0c8bc8741bcb7d1a83d2fba53
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-27 13:53:26 -0400

    request data from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-// test
+var stockDataApiToken = "VQxLXn7DNMOmzB4lAV4OxmImM24GRRLXLHOfAZtv";
+var stockDataSymbolPlaceholder = "TSLA,APPL"
+
+var getUserStock = function(userStock) {
+    // format the api url
+    var stockDataApiUrl = "https://api.stockdata.org/v1/data/quote?symbols=" + stockDataSymbolPlaceholder + "&api_token=" + stockDataApiToken ;
+
+    // make a request to the url
+    fetch(stockDataApiUrl)
+        .then(function(response) {
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        });
+};
+
+getUserStock();
