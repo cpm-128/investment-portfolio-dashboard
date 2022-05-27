cf8f06137be2d29e22852d45a0979583aec91bda
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-27 15:45:59 -0400

    in pprogress form handler and error handling for first stock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
-var stockDataSymbolPlaceholder = "TSLA,APPL"
+var stockSymbolPlaceholder = "TSLA"
 var userStockFormEl = document.querySelector("#stockForm1");
 var stockNameInputEl = document.querySelector("#inline-stock-name");
 var holdingsInputEl = document.querySelector("#inline-shares");
 
 var getUserStock = function(userStock) {
     // format the api url
-    var stockDataApiUrl = "https://api.stockdata.org/v1/data/quote?symbols=" + stockDataSymbolPlaceholder + "&api_token=VQxLXn7DNMOmzB4lAV4OxmImM24GRRLXLHOfAZtv" ;
+    var yhFinanceApiUrl = "https://yh-finance.p.rapidapi.com/stock/v3/get-chart?interval=5m&symbol=" + stockSymbolPlaceholder + "&range=1d&region=US&includePrePost=false&useYfid=true&includeAdjustedClose=true&events=capitalGain%2Cdiv%2Csplit" ;
+
+    // headers info from yhFinance
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
+            'X-RapidAPI-Key': '895f022c33msh5649ec36fc22d45p1991b5jsn5881cd33897c'
+        }
+    };
 
     // make a request to the url
-    fetch(stockDataApiUrl)
+    fetch(yhFinanceApiUrl, options)
         .then(function(response) {
             response.json().then(function(data) {
                 console.log(data);
@@ -18,7 +27,22 @@ var getUserStock = function(userStock) {
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
-    console.log(">>>" , event);
+
+    // get value from stock input element
+    var stockName = stockNameInputEl.value.trim();
+
+    if (stockName) {
+        getUserStock(stockName);
+        console.log(">>> remove error message >>>")
+    } else {
+        console.log("ERROR >>>")
+        var stockNameErrorEl = document.createElement("p");
+        stockNameErrorEl.textContent = "Enter a valid stock name such as APPL or TSLA.";
+        stockNameErrorEl.setAttribute("class" , "error-message");
+        userStockFormEl.append(stockNameErrorEl);
+    }
+
+    // **TO ADD: get value from # holdings element and pass to performance calculation
 };
 
 userStockFormEl.addEventListener("submit" , formSubmitHandler);
