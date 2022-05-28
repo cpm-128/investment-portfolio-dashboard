5a643233ae7c717ce080a82db8ca257c7b44676b
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-27 23:55:36 -0400

    color code individual stock performance

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var userStockFormEl = document.querySelector("#stockForm1");
 var stockNameInputEl = document.querySelector("#inline-stock-name");
 var holdingsInputEl = document.querySelector("#inline-shares");
 var inputErrorEl = document.querySelector("#input-error");
+var displayStock1El = document.querySelector("#display-stock-1");
 
 
 
@@ -20,10 +21,38 @@ var getUserStock = function(stockName) {
     // make a request to the url
     fetch(yhFinanceApiUrl, options)
         .then(function(response) {
-            response.json().then(function(data) {
-                console.log(">> regular market price >>" , data.price.regularMarketPrice.raw);
-                console.log(">> regular market change % >>" , data.price.regularMarketChangePercent.fmt);
+            if(response.ok) {
+                response.json().then(function(data) {
+                var regMarketPrice = data.price.regularMarketPrice.raw;
+                var regMarketChange = data.price.regularMarketChangePercent.raw;
+                    console.log(">> regular market price >>" , regMarketPrice);
+                    console.log(">> regular market change % >>" , regMarketChange);
+
+                // append to page
+                var stockNameEl = document.createElement("h3");
+                    stockNameEl.textContent = stockName.toUpperCase();
+                    stockNameEl.setAttribute = ("class" , "w-full max-w-sm align-center");
+                    displayStock1El.append(stockNameEl);
+                var regMarketPriceEl = document.createElement("p");
+                    regMarketPriceEl.textContent = "$" + regMarketPrice;
+                    stockNameEl.setAttribute = ("class" , "w-full max-w-sm");
+                    displayStock1El.append(regMarketPriceEl);
+                var regMarketChangeEl = document.createElement("p");
+                    regMarketChangeEl.textContent = (regMarketChange*100).toFixed(2) + "%";
+                    stockNameEl.setAttribute = ("class" , "w-full max-w-sm");
+                    displayStock1El.append(regMarketChangeEl);
+
+                // color code displayStock1El
+                if (regMarketChange > 0) {
+                    displayStock1El.setAttribute("class" , "performance-positive");
+                    console.log("POSITIVE");
+                } else if (regMarketChange < 0) {
+                    displayStock1El.setAttribute("class" , "performance-negative");
+                    console.log("NEGATIVE");
+                }
+
             });
+            }
         });
 };
 
@@ -35,8 +64,9 @@ var formSubmitHandler = function(event) {
 
     if (stockName) {
         getUserStock(stockName);
-        // remove any error message
+        // remove any error message and any existing stock display
         inputErrorEl.innerHTML = "";
+        displayStock1El.innerHTML = "";
     } else {
         var stockNameErrorEl = document.createElement("p");
         stockNameErrorEl.textContent = "Enter a valid stock name such as AAPL or TSLA.";
