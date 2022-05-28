ef5527037d532239e9e0690988d2f4190b5601df
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-28 00:41:06 -0400

    very not DRY but completely functioning api calls & display for user stocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,3 +167,258 @@ var formSubmitHandler2 = function(event) {
 
 userStockForm2El.addEventListener("submit" , formSubmitHandler2);
 // END STOCK 2
+
+// START STOCK 3
+var userStockForm3El = document.querySelector("#stockForm3");
+var stockNameInput3El = document.querySelector("#stock-input-3");
+var sharesHeldInput3El = document.querySelector("#share-input-3");
+var inputError3El = document.querySelector("#input-error-3");
+var displayStock3El = document.querySelector("#display-stock-3");
+
+var getUserStock3 = function(stockName) {
+    // format the api url
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
+            'X-RapidAPI-Key': '895f022c33msh5649ec36fc22d45p1991b5jsn5881cd33897c'
+        }
+    };
+    var yhFinanceApiUrl = "https://yh-finance.p.rapidapi.com/stock/v2/get-summary?symbol=" + stockName + "&region=US" ;
+
+    // make a request to the url
+    fetch(yhFinanceApiUrl, options)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                var regMarketPrice = data.price.regularMarketPrice.raw;
+                var regMarketChange = data.price.regularMarketChangePercent.raw;
+                    console.log(">> regular market price >>" , regMarketPrice);
+                    console.log(">> regular market change % >>" , regMarketChange);
+
+                // append to page
+                var stockNameEl = document.createElement("h3");
+                    stockNameEl.textContent = stockName.toUpperCase();
+                    stockNameEl.setAttribute = ("class" , "w-full max-w-sm align-center");
+                    displayStock3El.append(stockNameEl);
+                var regMarketPriceEl = document.createElement("p");
+                    regMarketPriceEl.textContent = "$" + regMarketPrice;
+                    stockNameEl.setAttribute = ("class" , "w-full max-w-sm");
+                    displayStock3El.append(regMarketPriceEl);
+                var regMarketChangeEl = document.createElement("p");
+                    regMarketChangeEl.textContent = (regMarketChange*100).toFixed(2) + "%";
+                    stockNameEl.setAttribute = ("class" , "w-full max-w-sm");
+                    displayStock3El.append(regMarketChangeEl);
+
+                // color code displayStock3El
+                if (regMarketChange > 0) {
+                    displayStock3El.setAttribute("class" , "performance-positive");
+                    console.log("POSITIVE");
+                } else if (regMarketChange < 0) {
+                    displayStock3El.setAttribute("class" , "performance-negative");
+                    console.log("NEGATIVE");
+                }
+
+            });
+            }
+        });
+};
+
+var formSubmitHandler3 = function(event) {
+    event.preventDefault();
+
+    // get value from stock input element
+    var stockName = stockNameInput3El.value.trim();
+
+    if (stockName) {
+        getUserStock3(stockName);
+        // remove any error message and any existing stock display
+        inputError3El.innerHTML = "";
+        displayStock3El.innerHTML = "";
+    } else {
+        var stockNameErrorEl = document.createElement("p");
+        stockNameErrorEl.textContent = "Enter a valid stock name such as AAPL or TSLA.";
+        stockNameErrorEl.setAttribute("class" , "error-message");
+        inputError3El.append(stockNameErrorEl);
+    }
+
+    // get value from shares input element
+    var sharesHeld = sharesHeldInput3El.value.trim();
+
+    if (sharesHeld) {
+        // **TO ADD: pass sharesHeld to performance calculation
+    };
+};
+
+userStockForm3El.addEventListener("submit" , formSubmitHandler3);
+// END STOCK 3
+
+// START STOCK 4
+var userStockForm4El = document.querySelector("#stockForm4");
+var stockNameInput4El = document.querySelector("#stock-input-4");
+var sharesHeldInput4El = document.querySelector("#share-input-4");
+var inputError4El = document.querySelector("#input-error-4");
+var displayStock4El = document.querySelector("#display-stock-4");
+
+var getUserStock4 = function(stockName) {
+    // format the api url
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
+            'X-RapidAPI-Key': '895f022c33msh5649ec36fc22d45p1991b5jsn5881cd33897c'
+        }
+    };
+    var yhFinanceApiUrl = "https://yh-finance.p.rapidapi.com/stock/v2/get-summary?symbol=" + stockName + "&region=US" ;
+
+    // make a request to the url
+    fetch(yhFinanceApiUrl, options)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                var regMarketPrice = data.price.regularMarketPrice.raw;
+                var regMarketChange = data.price.regularMarketChangePercent.raw;
+                    console.log(">> regular market price >>" , regMarketPrice);
+                    console.log(">> regular market change % >>" , regMarketChange);
+
+                // append to page
+                var stockNameEl = document.createElement("h3");
+                    stockNameEl.textContent = stockName.toUpperCase();
+                    stockNameEl.setAttribute = ("class" , "w-full max-w-sm align-center");
+                    displayStock4El.append(stockNameEl);
+                var regMarketPriceEl = document.createElement("p");
+                    regMarketPriceEl.textContent = "$" + regMarketPrice;
+                    stockNameEl.setAttribute = ("class" , "w-full max-w-sm");
+                    displayStock4El.append(regMarketPriceEl);
+                var regMarketChangeEl = document.createElement("p");
+                    regMarketChangeEl.textContent = (regMarketChange*100).toFixed(2) + "%";
+                    stockNameEl.setAttribute = ("class" , "w-full max-w-sm");
+                    displayStock4El.append(regMarketChangeEl);
+
+                // color code displayStock4El
+                if (regMarketChange > 0) {
+                    displayStock4El.setAttribute("class" , "performance-positive");
+                    console.log("POSITIVE");
+                } else if (regMarketChange < 0) {
+                    displayStock4El.setAttribute("class" , "performance-negative");
+                    console.log("NEGATIVE");
+                }
+
+            });
+            }
+        });
+};
+
+var formSubmitHandler4 = function(event) {
+    event.preventDefault();
+
+    // get value from stock input element
+    var stockName = stockNameInput4El.value.trim();
+
+    if (stockName) {
+        getUserStock4(stockName);
+        // remove any error message and any existing stock display
+        inputError4El.innerHTML = "";
+        displayStock4El.innerHTML = "";
+    } else {
+        var stockNameErrorEl = document.createElement("p");
+        stockNameErrorEl.textContent = "Enter a valid stock name such as AAPL or TSLA.";
+        stockNameErrorEl.setAttribute("class" , "error-message");
+        inputError4El.append(stockNameErrorEl);
+    }
+
+    // get value from shares input element
+    var sharesHeld = sharesHeldInput4El.value.trim();
+
+    if (sharesHeld) {
+        // **TO ADD: pass sharesHeld to performance calculation
+    };
+};
+
+userStockForm4El.addEventListener("submit" , formSubmitHandler4);
+// END STOCK 4
+
+// START STOCK 5
+var userStockForm5El = document.querySelector("#stockForm5");
+var stockNameInput5El = document.querySelector("#stock-input-5");
+var sharesHeldInput5El = document.querySelector("#share-input-5");
+var inputError5El = document.querySelector("#input-error-5");
+var displayStock5El = document.querySelector("#display-stock-5");
+
+var getUserStock5 = function(stockName) {
+    // format the api url
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
+            'X-RapidAPI-Key': '895f022c33msh5649ec36fc22d45p1991b5jsn5881cd33897c'
+        }
+    };
+    var yhFinanceApiUrl = "https://yh-finance.p.rapidapi.com/stock/v2/get-summary?symbol=" + stockName + "&region=US" ;
+
+    // make a request to the url
+    fetch(yhFinanceApiUrl, options)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                var regMarketPrice = data.price.regularMarketPrice.raw;
+                var regMarketChange = data.price.regularMarketChangePercent.raw;
+                    console.log(">> regular market price >>" , regMarketPrice);
+                    console.log(">> regular market change % >>" , regMarketChange);
+
+                // append to page
+                var stockNameEl = document.createElement("h3");
+                    stockNameEl.textContent = stockName.toUpperCase();
+                    stockNameEl.setAttribute = ("class" , "w-full max-w-sm align-center");
+                    displayStock5El.append(stockNameEl);
+                var regMarketPriceEl = document.createElement("p");
+                    regMarketPriceEl.textContent = "$" + regMarketPrice;
+                    stockNameEl.setAttribute = ("class" , "w-full max-w-sm");
+                    displayStock5El.append(regMarketPriceEl);
+                var regMarketChangeEl = document.createElement("p");
+                    regMarketChangeEl.textContent = (regMarketChange*100).toFixed(2) + "%";
+                    stockNameEl.setAttribute = ("class" , "w-full max-w-sm");
+                    displayStock5El.append(regMarketChangeEl);
+
+                // color code displayStock5El
+                if (regMarketChange > 0) {
+                    displayStock5El.setAttribute("class" , "performance-positive");
+                    console.log("POSITIVE");
+                } else if (regMarketChange < 0) {
+                    displayStock5El.setAttribute("class" , "performance-negative");
+                    console.log("NEGATIVE");
+                }
+
+            });
+            }
+        });
+};
+
+var formSubmitHandler5 = function(event) {
+    event.preventDefault();
+
+    // get value from stock input element
+    var stockName = stockNameInput5El.value.trim();
+
+    if (stockName) {
+        getUserStock5(stockName);
+        // remove any error message and any existing stock display
+        inputError5El.innerHTML = "";
+        displayStock5El.innerHTML = "";
+    } else {
+        var stockNameErrorEl = document.createElement("p");
+        stockNameErrorEl.textContent = "Enter a valid stock name such as AAPL or TSLA.";
+        stockNameErrorEl.setAttribute("class" , "error-message");
+        inputError5El.append(stockNameErrorEl);
+    }
+
+    // get value from shares input element
+    var sharesHeld = sharesHeldInput5El.value.trim();
+
+    if (sharesHeld) {
+        // **TO ADD: pass sharesHeld to performance calculation
+    };
+};
+
+userStockForm5El.addEventListener("submit" , formSubmitHandler5);
+// END STOCK 5
