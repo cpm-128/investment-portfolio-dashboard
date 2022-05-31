cc1d81ad61e64af39175a0aed009f7f892197448
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-31 18:57:29 -0400

    updated api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ var getUserStock1 = function(stockName) {
         method: 'GET',
         headers: {
             'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
-            'X-RapidAPI-Key': '895f022c33msh5649ec36fc22d45p1991b5jsn5881cd33897c'
+            'X-RapidAPI-Key': 'b239052929msh610a70ac1b2dcccp119d5bjsna0af27722d38'
         }
     };
     var yhFinanceApiUrl = "https://yh-finance.p.rapidapi.com/stock/v2/get-summary?symbol=" + stockName + "&region=US" ;
@@ -103,7 +103,7 @@ var getUserStock2 = function(stockName) {
         method: 'GET',
         headers: {
             'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
-            'X-RapidAPI-Key': '895f022c33msh5649ec36fc22d45p1991b5jsn5881cd33897c'
+            'X-RapidAPI-Key': 'b239052929msh610a70ac1b2dcccp119d5bjsna0af27722d38'
         }
     };
     var yhFinanceApiUrl = "https://yh-finance.p.rapidapi.com/stock/v2/get-summary?symbol=" + stockName + "&region=US" ;
@@ -195,7 +195,7 @@ var getUserStock3 = function(stockName) {
         method: 'GET',
         headers: {
             'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
-            'X-RapidAPI-Key': '895f022c33msh5649ec36fc22d45p1991b5jsn5881cd33897c'
+            'X-RapidAPI-Key': 'b239052929msh610a70ac1b2dcccp119d5bjsna0af27722d38'
         }
     };
     var yhFinanceApiUrl = "https://yh-finance.p.rapidapi.com/stock/v2/get-summary?symbol=" + stockName + "&region=US" ;
@@ -287,7 +287,7 @@ var getUserStock4 = function(stockName) {
         method: 'GET',
         headers: {
             'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
-            'X-RapidAPI-Key': '895f022c33msh5649ec36fc22d45p1991b5jsn5881cd33897c'
+            'X-RapidAPI-Key': 'b239052929msh610a70ac1b2dcccp119d5bjsna0af27722d38'
         }
     };
     var yhFinanceApiUrl = "https://yh-finance.p.rapidapi.com/stock/v2/get-summary?symbol=" + stockName + "&region=US" ;
@@ -379,7 +379,7 @@ var getUserStock5 = function(stockName) {
         method: 'GET',
         headers: {
             'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
-            'X-RapidAPI-Key': '895f022c33msh5649ec36fc22d45p1991b5jsn5881cd33897c'
+            'X-RapidAPI-Key': 'b239052929msh610a70ac1b2dcccp119d5bjsna0af27722d38'
         }
     };
     var yhFinanceApiUrl = "https://yh-finance.p.rapidapi.com/stock/v2/get-summary?symbol=" + stockName + "&region=US" ;
@@ -463,15 +463,15 @@ var portfolioPerformanceEl = document.querySelector("#portfolio-performance-calc
 
 var calculatePortfolioPerformance = function() {
 
-    var regMarketChange1 = parseInt(localStorage.getItem("regMarketChange1"));
+    var regMarketChange1 = localStorage.getItem("regMarketChange1");
     var sharesHeld1 = parseInt(localStorage.getItem("sharesHeld1"));
-    var regMarketChange2 = parseInt(localStorage.getItem("regMarketChange2"));
+    var regMarketChange2 = localStorage.getItem("regMarketChange2");
     var sharesHeld2 = parseInt(localStorage.getItem("sharesHeld2"));
-    var regMarketChange3 = parseInt(localStorage.getItem("regMarketChange3"));
+    var regMarketChange3 = localStorage.getItem("regMarketChange3");
     var sharesHeld3 = parseInt(localStorage.getItem("sharesHeld3"));
-    var regMarketChange4 = parseInt(localStorage.getItem("regMarketChange4"));
+    var regMarketChange4 = localStorage.getItem("regMarketChange4");
     var sharesHeld4 = parseInt(localStorage.getItem("sharesHeld4"));
-    var regMarketChange5 = parseInt(localStorage.getItem("regMarketChange5"));
+    var regMarketChange5 = localStorage.getItem("regMarketChange5");
     var sharesHeld5 = parseInt(localStorage.getItem("sharesHeld5"));
 
     // calculation
